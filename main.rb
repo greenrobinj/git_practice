6d8cd09fe6d0551bcb7058f6e8fc07f49a9bbfea
Robin Green <greenrj4@gmail.com>
2018-05-10 11:19:58 -0600

    test commit

---
+++ main.rb
@@ -0,0 +1,21 @@
+def puts_gits(cmd)
+  puts `git #{cmd} -h`
+end
+
+def menu
+  puts '1: Inter git command'
+  puts '2: Exit'
+
+
+case choice
+  when 1
+  puts 'Enter a git comand'
+  puts_gits(gets.strip)
+  menu
+  when 2
+  Exit
+  else
+  puts ' Invalid Choice'
+  menu
+  end
+end
